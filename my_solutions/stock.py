b2e908f3e743c0103f683ad13d24ff35f6f0982c
David Dominguez <352985+ddominguez@users.noreply.github.com>
2024-08-08 22:24:10 -0400

    added ex 3.1

--- my_solutions/stock.py
+++ my_solutions/stock.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Stock:
     def __init__(self, name, shares, price):
         self.name = name
@@ -6,3 +9,27 @@ class Stock:
 
     def cost(self):
         return self.shares * self.price
+
+    def sell(self, shares):
+        self.shares -= shares
+
+
+def read_portfolio(portfolio_file):
+    """Reads a file of portfolio data into a list of Stock objects."""
+
+    stocks = []
+    with open(portfolio_file) as f:
+        rows = csv.reader(f)
+        _ = next(rows)
+        for name, shares, price in rows:
+            stocks.append(Stock(name, int(shares), float(price)))
+    return stocks
+
+
+def print_portfolio(stocks):
+    """Print portfolio in a formatted table."""
+
+    print("%10s %10s %10s" % ("name", "shares", "price"))
+    print((("-" * 10) + " ") * 3)
+    for s in stocks:
+        print("%10s %10d %10.2f" % (s.name, s.shares, s.price))
