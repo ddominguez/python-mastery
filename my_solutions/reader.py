07e9762c248b3cb5a625c102512b405687f489ef
David Dominguez <352985+ddominguez@users.noreply.github.com>
2024-08-12 17:41:48 -0400

    Exercise 3.3
    Added from_row class method to Stock class.

--- my_solutions/reader.py
+++ my_solutions/reader.py
@@ -1,7 +1,15 @@
+"""
+Python Mastery
+
+Exercise 3.3
+- added read_csv_as_instances()
+"""
+
 import csv
 
 
 def read_csv_as_dicts(filename: str, coltypes: list):
+    """Read a CSV file into a list of dicts."""
     records = []
     with open(filename) as f:
         rows = csv.reader(f)
@@ -11,4 +19,16 @@ def read_csv_as_dicts(filename: str, coltypes: list):
                 {name: func(val) for name, func, val in zip(headers, coltypes, row)}
             )
 
+    return records
+
+
+def read_csv_as_instances(filename, cls):
+    """Read a CSV file into a list of instances."""
+
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        _ = next(rows)
+        for row in rows:
+            records.append(cls.from_row(row))
     return records
--- my_solutions/stock.py
+++ my_solutions/stock.py
@@ -1,7 +1,20 @@
-import csv
+"""
+Python Mastery
+
+Exercise 3.3
+- added from_row class method
+- removed read_portfolio()
+"""
 
 
 class Stock:
+    types = (str, int, float)
+
+    @classmethod
+    def from_row(cls, row):
+        values = [func(val) for func, val in zip(cls.types, row)]
+        return cls(*values)
+
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
@@ -12,18 +25,6 @@ class Stock:
 
     def sell(self, shares):
         self.shares -= shares
-
-
-def read_portfolio(portfolio_file):
-    """Reads a file of portfolio data into a list of Stock objects."""
-
-    stocks = []
-    with open(portfolio_file) as f:
-        rows = csv.reader(f)
-        _ = next(rows)
-        for name, shares, price in rows:
-            stocks.append(Stock(name, int(shares), float(price)))
-    return stocks
 
 
 def print_portfolio(stocks):
