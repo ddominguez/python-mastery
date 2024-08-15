1cb1a6cbbb907c160293b7be123f22e6efd5a0f2
David Dominguez <352985+ddominguez@users.noreply.github.com>
2024-08-14 22:13:07 -0400

    Exercise 3.5

--- my_solutions/tableformat.py
+++ my_solutions/tableformat.py
@@ -2,17 +2,63 @@
 Python Mastery
 
 Exercise 3.2
+- added print_table()
+
+
+Exercise 3.5
+- added TableFormatter classes
 """
 
-COLUMN_WIDTH = 10
+
+class TableFormatter:
+    def headings(self, headers):
+        raise NotImplementedError
+
+    def row(self, rowdata):
+        raise NotImplementedError
 
 
-def print_table(objects: list[object], attributes: list[str]):
-    """
-    Takes a sequence (list) of objects, a list of attribute names,
-     and prints a nicely formatted table.
-    """
-    print(" ".join(f"{attr:>{COLUMN_WIDTH}}" for attr in attributes))
-    print(" ".join("-" * COLUMN_WIDTH for _ in range(len(attributes))))
-    for obj in objects:
-        print(" ".join(f"{getattr(obj, attr):>{COLUMN_WIDTH}}" for attr in attributes))
+class TextTableFormatter(TableFormatter):
+    def headings(self, headers):
+        print(" ".join(f"{h:>10}" for h in headers))
+        print(" ".join("-" * 10 for _ in range(len(headers))))
+
+    def row(self, rowdata):
+        print(" ".join(f"{d:>10}" for d in rowdata))
+
+
+class CSVTableFormatter:
+    def headings(self, headers):
+        print(",".join(h for h in headers))
+
+    def row(self, rowdata):
+        print(",".join(f"{d}" for d in rowdata))
+
+
+class HTMLTableFormatter:
+    def headings(self, headers):
+        print("<tr>", " ".join(f"<th>{h}</th>" for h in headers), "</tr>")
+
+    def row(self, rowdata):
+        print("<tr>", " ".join(f"<td>{d}</td>" for d in rowdata), "</tr>")
+
+
+def create_formatter(name: str):
+    """Returns a table formatter."""
+    if name == "text":
+        formatter_cls = TextTableFormatter
+    elif name == "csv":
+        formatter_cls = CSVTableFormatter
+    elif name == "html":
+        formatter_cls = HTMLTableFormatter
+    else:
+        raise ValueError("expected text, csv, or html")
+    return formatter_cls()
+
+
+def print_table(records, fields, formatter):
+    """Prints a nicely formatted table."""
+    formatter.headings(fields)
+    for r in records:
+        rowdata = [getattr(r, fieldname) for fieldname in fields]
+        formatter.row(rowdata)
